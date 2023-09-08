71854b7871d2d7b2c3d55f347e638c6e4b5c1555
odikadzegiorgi4@gmail.com <odikadzegiorgi4@gmail.com>
2023-09-08 16:21:00 +0400

    change user context to redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from "./routes/home/home.component";
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from './routes/authentication/authentication.component'; 
-import Shop from './routes/shop-page/shop.component';
-import Checkout from './routes/checkout/checkout.component';
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop-page/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
+
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "./utils/firebase/firebase.utils";
+
+import { createAction } from "./utils/reducer/reducer.utils";
+
+import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
-  return(
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
     <Routes>
-      <Route path='/' element={<Navigation/>}>
-        <Route index element={<Home/>}/>
-        <Route path='shop/*' element={<Shop/>}/>
-        <Route path='auth' element={<Authentication/>}/>
-        <Route path='checkout' element={<Checkout/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
-  )
+  );
 };
 
 export default App;
